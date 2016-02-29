1e7316dd351552ac119645dec31d5c6679b7634a
Michael Handschuh <mhandschuh@gmail.com>
2016-02-28 22:43:01 -0500

    Adds BasicTemplateOptionsAlgorithm

---
+++ Algorithm.CSharp/BasicTemplateOptionsAlgorithm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuantConnect.Data;
+using QuantConnect.Data.Market;
+
+namespace QuantConnect.Algorithm.CSharp
+{
+    public class BasicTemplateOptionsAlgorithm : QCAlgorithm
+    {
+        private const string UnderlyingTicker = "GOOG";
+        public readonly Symbol Underlying = QuantConnect.Symbol.Create(UnderlyingTicker, SecurityType.Equity, Market.USA);
+        public readonly Symbol OptionSymbol = QuantConnect.Symbol.Create(UnderlyingTicker, SecurityType.Option, Market.USA);
+        public override void Initialize()
+        {
+            SetStartDate(2015, 12, 24);
+            SetEndDate(2015, 12, 24);
+            SetCash(10000);
+
+            AddEquity(UnderlyingTicker);
+            AddOption(UnderlyingTicker);
+        }
+
+        public override void OnData(Slice slice)
+        {
+            if (!Portfolio.Invested)
+            {
+                OptionChain chain;
+                if (slice.OptionChains.TryGetValue(OptionSymbol, out chain))
+                {
+                    // find the second call strike under market price expiring today
+                    var contract = (
+                        from optionContract in chain.OrderByDescending(x => x.Strike)
+                        where optionContract.Right == OptionRight.Call
+                        where optionContract.Strike < chain.Underlying.Price
+                        select optionContract
+                        ).Skip(2).FirstOrDefault();
+
+                    if (contract != null)
+                    {
+                        SetHoldings(contract.Symbol, 1);
+                    }
+                }
+            }
+        }
+    }
+}
