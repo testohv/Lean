6c29d6aeb1b145fd0accefa233b802bd6aadb330
Michael Handschuh <mhandschuh@gmail.com>
2016-03-01 19:30:42 -0500

    Add comments/banner to BasicTemplateOptionsAlgorithm

--- Algorithm.CSharp/BasicTemplateOptionsAlgorithm.cs
+++ Algorithm.CSharp/BasicTemplateOptionsAlgorithm.cs
@@ -1,18 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using QuantConnect.Data;
 using QuantConnect.Data.Market;
 
 namespace QuantConnect.Algorithm.CSharp
 {
+    /// <summary>
+    /// This example demonstrates how to add options for a given underlying equity security.
+    /// It also shows how you can prefilter contracts easily based on strikes and expirations.
+    /// It also shows how you can inspect the option chain to pick a specific option contract to trade.
+    /// </summary>
     public class BasicTemplateOptionsAlgorithm : QCAlgorithm
     {
         private const string UnderlyingTicker = "GOOG";
         public readonly Symbol Underlying = QuantConnect.Symbol.Create(UnderlyingTicker, SecurityType.Equity, Market.USA);
         public readonly Symbol OptionSymbol = QuantConnect.Symbol.Create(UnderlyingTicker, SecurityType.Option, Market.USA);
+
         public override void Initialize()
         {
             SetStartDate(2015, 12, 24);
@@ -23,6 +41,10 @@ namespace QuantConnect.Algorithm.CSharp
             AddOption(UnderlyingTicker);
         }
 
+        /// <summary>
+        /// Event - v3.0 DATA EVENT HANDLER: (Pattern) Basic template for user to override for receiving all subscription data in a single event
+        /// </summary>
+        /// <param name="slice">The current slice of data keyed by symbol string</param>
         public override void OnData(Slice slice)
         {
             if (!Portfolio.Invested)
